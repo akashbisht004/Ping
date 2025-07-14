0739fbdc06d2cd254d7b015eb129d89c3b4af89f
Akash Bisht <bisht26akash@gmail.com>
2025-07-14 14:41:20 +0530

    webrtc

--- backend/index.js
+++ backend/index.js
@@ -9,12 +9,22 @@ const socktIdToEmailMap=new Map();
 
 io.on("connection",socket=>{
     console.log('Socket Connected',socket.id)
-    socket.on('room:join',data=>{
+    socket.on('room:join', (data)=>{
         const {email,room}=data;
         emailToSocketIdMap.set(email,socket.id);
         socktIdToEmailMap.set(socket.id,email);
-        io.to(room).emit('user:joined',{email,id:socket.id})
+        io.to(room).emit('user:joined',{email, id:socket.id})
         socket.join(room);
         io.to(socket.id).emit('room:join',data);
     })
+
+    socket.on("user:call", ({to,offer})=>{
+        io.to(to).emit('incoming:call',{from:socket.id, offer})
+    })
+
+    socket.on("call:accepted",({to: from,ans})=>{
+        io.to(to).emit("call:accepted",{from:socket.id, ans})
+    })
+
+    socket.on('')
 })
